e0845370c1be2024be8c0f6614bca92f2da35a92
William Carolin [Contractor] <wcarolin@ithaa1wcarolin.local>
2018-06-29 17:15:28 -0400

    Added MIT license.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject artstor-group-service "0.0.7-SNAPSHOT"
   :description "AIW Group Service"
   :url "http://www.artstor.org/"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/data.codec "0.1.0"]
